843b8b4a258eefd0582b5dbd92049e4f7f8c9089
uttamXD <raiuttam88.ur@gmail.com>
2023-06-14 21:04:21 +0545

    Completed project profile_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +11,48 @@ void main() {
         centerTitle: true,
         title: const Text('Profile App'),
       ),
-      body: Column(Image.asset('assets/images/me.jpg')),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          Center(
+              child: Image.asset(
+            'assets/images/me.jpg',
+            height: 250,
+            width: 250,
+          )),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            'Name:Uttam Chamling Rai',
+            style: TextStyle(
+                fontSize: 23, color: Colors.green, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            'Address:Bhaktapur, Nepal',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            'Email: raiuttam88.ur@gmail.com',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(
+            height: 90,
+          ),
+          Text(
+            'Developed By: Uttam Chamling Rai',
+            style: TextStyle(fontSize: 20),
+          ),
+        ],
+      ),
     ),
   ));
 }
